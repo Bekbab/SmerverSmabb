4d4480cc5338fbb660114481c506092ec1efab0b
Bekbab <72729623+Bekbab@users.noreply.github.com>
2021-12-01 14:23:55 +0100

    first commit

---
+++ SmerverSmabb/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using RestSharp;
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+RestRequest pokeRequest = new RestRequest("pokemon/mismagius");
+IRestResponse response = pokeClient.Get(pokeRequest);
+
+Console.WriteLine(response.StatusCode);
+
+Console.ReadLine();
